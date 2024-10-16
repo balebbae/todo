84c71d78fe58de3ad92f6d6a3500445a0979fdd5
Caleb Bae <baecal000@gmail.com>
2024-10-16 15:06:03 -0500

    Update and Delete

--- app/todo/actions.ts
+++ app/todo/actions.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { Todo } from "@/types/custom";
 import { createClient } from "@/utils/supabase/server";
 import { revalidatePath } from "next/cache";
 
@@ -27,5 +28,43 @@ export async function addTodo(formData: FormData) {
     throw new Error(`Error adding task: ${error.message}`);
   }
 
+  revalidatePath("/todo");
+}
+
+export async function deleteTodo(id: number) {
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (!user) {
+    throw new Error("User is not logged in");
+  }
+  const { error } = await supabase
+    .from("todos")
+    .delete()
+    .match({ user_id: user.id, id: id });
+  if (error) {
+    console.error("Supabase delete error:", error.message); // Log the actual error
+    throw new Error(`Error deleting task: ${error.message}`);
+  }
+  revalidatePath("/todo");
+}
+
+export async function updateTodo(todo: Todo) {
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (!user) {
+    throw new Error("User is not logged in");
+  }
+  const { error } = await supabase
+    .from("todos")
+    .update(todo)
+    .match({ user_id: user.id, id: todo.id });
+  if (error) {
+    console.error("Supabase update error:", error.message); // Log the actual error
+    throw new Error(`Error updating task: ${error.message}`);
+  }
   revalidatePath("/todo");
 }
--- components/todo-item.tsx
+++ components/todo-item.tsx
@@ -1,9 +1,12 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Checkbox } from "@/components/ui/checkbox";
 import { cn } from "@/lib/utils";
 import { Trash2 } from "lucide-react";
 import { Todo } from "@/types/custom";
+import { deleteTodo, updateTodo } from "@/app/todo/actions";
 
 export function TodoItem({ todo }: { todo: Todo }) {
   return (
@@ -18,10 +21,20 @@ export function TodoCard({ todo }: { todo: Todo }) {
     <Card className={cn("w-full")}>
       <CardContent className="flex items-start gap-3 p-3">
         <span className="size-10 flex items-center justify-center">
-          <Checkbox />
+          <Checkbox
+            checked={Boolean(todo.is_completed)}
+            onCheckedChange={async (val) => {
+              if (val === null) return; // If you're supporting an indeterminate state, this can be used
+              await updateTodo({ ...todo, is_completed: Boolean(val) });
+            }}
+          />
         </span>
         <p className={cn("flex-1 pt-2 min-w-0 break-words")}>{todo.task}</p>
-        <Button variant="ghost" size="icon">
+        <Button
+          formAction={async () => await deleteTodo(todo.id)}
+          variant="ghost"
+          size="icon"
+        >
           <Trash2 className="h-5 w-5" />
           <span className="sr-only">Delete Todo</span>
         </Button>
